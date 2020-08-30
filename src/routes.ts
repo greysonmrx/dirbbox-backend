b8cef7a8ecb3521a5bcaffe7041bcc7f3d150190
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-30 18:19:15 -0300

    feat: create middleware to ensure user authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import UsersController from './controllers/UsersController';
 import SessionsController from './controllers/SessionsController';
 
+import ensureAuthenticated from './middleware/ensureAuthenticated';
+
 const routes = Router();
 const usersController = new UsersController();
 const sessionsController = new SessionsController();
@@ -12,5 +14,7 @@ routes.post('/users', usersController.store);
 
 // Sessions
 routes.post('/sessions', sessionsController.store);
+
+routes.use(ensureAuthenticated);
 
 export default routes;
