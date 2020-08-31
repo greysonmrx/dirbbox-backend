895251efe3e562eef149e7f470975a1a2e0820bc
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-30 22:00:20 -0300

    feat: add celebrate

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 
@@ -8,5 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.listen(5000);
